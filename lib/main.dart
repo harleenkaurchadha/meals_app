f6ae7fbfe626ef8cbaa4e934f2e1e3c72eab5b9c
Harleen Kaur <harleen198.chadha@gmail.com>
2020-06-15 02:07:18 +0530

    grid view & navigating to selectedd category page

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme:ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.purple,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),    // background color
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          // ignore: deprecated_member_use
+          body1: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          // ignore: deprecated_member_use
+          body2: TextStyle(
+            color: Color.fromRGBO(20,51,51,1),
+          ),
+          // ignore: deprecated_member_use
+          title: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: CategoriesScreen(),
+      );
+
+  }
+}
