7f9a057a2bbe05bcb08483b71f2b0963bf13147c
Harleen Kaur <harleen198.chadha@gmail.com>
2020-06-23 01:47:01 +0530

    adding bottom TabBar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,39 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/screens/favourites_screen.dart';
+import './favourites_screen.dart';
 import './categories_screen.dart';
 
 class TabsScreen  extends StatefulWidget{
-
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
 
-class _TabsScreenState extends State<TabsScreen>{
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(),
+      'title': 'Categories'
+    },
+    {'page': FavouritesScreen(),
+      'title': 'Your Favourites'
+    },
+  ];
+  int _selectedPageIndex=0;
+
+  void _selectPage(int index) {         // flutter will give index of selected tab
+    setState(() {
+      _selectedPageIndex= index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(            //automatically detects which tab is selected & then show the content
-      length: 2,
-      initialIndex: 0,    //by default selected tab
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(tabs: <Widget>[
-            Tab(
-              icon: Icon(Icons.category),
-              text: 'Categories',
-            ),
-            Tab(
-                icon: Icon(Icons.star),
-                text: 'Favourites',
-            ),
-          ],),
-        ),
-        body: TabBarView(children: <Widget>[     // page view which displays widget corresponding to currently selected tab
-          CategoriesScreen(),
-          FavouritesScreen(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text('Favourites'),
+          ),
         ],
-        ),
+
       ),
     );
   }
